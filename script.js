0a77dfce927c78cadb9a654857d474f222f4db52
Dan Versalov <DanV@Dans-MacBook-Air.local>
2016-10-08 22:32:21 -0400

    Minor tweaks

--- script.js
+++ script.js
@@ -5,18 +5,42 @@ var hasClicked = false;
 var flag = 0;
 function counterClick1() {
 	if(flag == 0) {
+		function myFunction() {
+    	var x = document.createElement("IMG");
+    	x.setAttribute("src", "img/meme1.jpg");
+    	x.setAttribute("width", "236");
+    	x.setAttribute("width", "255");
+    	x.setAttribute("alt", "The Pulpit Rock");
+    	document.body.appendChild(x);
+}
 		updateCounter1();
 		flag = 1;
 	}
 };
 function counterClick2() {
 	if(flag == 0) {
+		function myFunction() {
+    	var x = document.createElement("IMG");
+    	x.setAttribute("src", "img_pulpit.jpg");
+    	x.setAttribute("width", "236");
+    	x.setAttribute("width", "255");
+    	x.setAttribute("alt", "The Pulpit Rock");
+    	document.body.appendChild(x);
+}
 		updateCounter2();
 		flag = 1;
 	}
 };
 function counterClick3() {
 	if(flag == 0) {
+		function myFunction() {
+    	var x = document.createElement("IMG");
+    	x.setAttribute("src", "img_pulpit.jpg");
+    	x.setAttribute("width", "304");
+    	x.setAttribute("width", "228");
+    	x.setAttribute("alt", "The Pulpit Rock");
+    	document.body.appendChild(x);
+}
 		updateCounter3();
 		flag = 1;
 	}
