fd00a121228d35bd9b4f3a6d7cfa8b2f7e304d2b
Dan Versalov <DanV@Dans-MacBook-Air.local>
2016-10-08 21:50:52 -0400

    Minor tweak

--- script.js
+++ script.js
@@ -22,30 +22,6 @@ function counterClick3() {
 	}
 };
 $(document).ready(function(){
-	if (fetchCounter1 > fetchCounter2) {
-		$('#car2').addClass('big');
-	}
-	else if () {
-
-	}
-	$('btn1').mouseenter(function(){
-
-	});
-	$('btn1').mouseleave(function(){
-
-	});
-	$('btn2').mouseenter(function(){
-
-	});
-	$('btn2').mouseleave(function(){
-		
-	});
-	$('btn3').mouseenter(function(){
-
-	});
-	$('btn3').mouseleave(function(){
-		
-	});
 	fetchCounters();
 	window.setInterval(fetchCounters, 1000);
 });
