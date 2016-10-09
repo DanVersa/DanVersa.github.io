e2e95c13b15ff2b756605fca97631d5e59b9ca97
Dan Versalov <DanV@Dans-MacBook-Air.local>
2016-10-08 23:36:23 -0400

    Minor tweaks

--- script.js
+++ script.js
@@ -5,47 +5,26 @@ var hasClicked = false;
 var flag = 0;
 function counterClick1() {
 	if(flag == 0) {
-		function myFunction() {
-    	var x = document.createElement("IMG");
-    	x.setAttribute("src", "img/meme1.jpg");
-    	x.setAttribute("width", "236");
-    	x.setAttribute("width", "255");
-    	x.setAttribute("alt", "The Pulpit Rock");
-    	document.body.appendChild(x);
-}
 		updateCounter1();
 		flag = 1;
 	}
 };
 function counterClick2() {
 	if(flag == 0) {
-		function myFunction() {
-    	var x = document.createElement("IMG");
-    	x.setAttribute("src", "img_pulpit.jpg");
-    	x.setAttribute("width", "236");
-    	x.setAttribute("width", "255");
-    	x.setAttribute("alt", "The Pulpit Rock");
-    	document.body.appendChild(x);
-}
 		updateCounter2();
 		flag = 1;
 	}
 };
 function counterClick3() {
 	if(flag == 0) {
-		function myFunction() {
-    	var x = document.createElement("IMG");
-    	x.setAttribute("src", "img_pulpit.jpg");
-    	x.setAttribute("width", "304");
-    	x.setAttribute("width", "228");
-    	x.setAttribute("alt", "The Pulpit Rock");
-    	document.body.appendChild(x);
-}
 		updateCounter3();
 		flag = 1;
 	}
 };
 $(document).ready(function(){
+	if (fetchCounter1() > fetchCounter2()) {
+		$('car1').addClass('big');
+	}
 	fetchCounters();
 	window.setInterval(fetchCounters, 1000);
 });
